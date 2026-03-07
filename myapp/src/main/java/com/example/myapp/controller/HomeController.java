e47d14f72f53d0cc5e050c85e995c77fab5df56b
mot1v217 <rudykh_d@list.ru>
2026-03-07 21:44:56 +0800

    Добавлен thymeleaf, JS и CSS теперь находятся в папках в static

--- myapp/src/main/java/com/example/myapp/controller/HomeController.java
+++ myapp/src/main/java/com/example/myapp/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.example.myapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     
     @GetMapping("/")
-    public String home() {
-        return "index";
+    public String home(Model model) {
+        // Добавляем данные, которые можно использовать в шаблоне
+        model.addAttribute("title", "Закон Гука");
+        model.addAttribute("description", "Имитация колебаний пружины");
+        return "index"; // имя шаблона (index.html)
     }
 }
